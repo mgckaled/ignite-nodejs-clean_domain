ce648c3c3ee3b5b2a31ea12cc90bf7a2a8b9a445
mgckaled <marcelgckaled@gmail.com>
2023-11-17 18:36:04 -0300

    feat: abstract entity creation

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from '../../core/entities/unique-entity-id'
 import { Answer } from '../entities/answer'
 import { type AnswersRepository } from '../repositories/answers-repository'
 
@@ -11,10 +12,10 @@ export class AnswerQuestionUseCase {
   constructor (private readonly answersRepository: AnswersRepository) {}
 
   async execute ({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({
+    const answer = Answer.create({
       content,
-      authorId: instructorId,
-      questionId
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId)
     })
 
     await this.answersRepository.create(answer)
