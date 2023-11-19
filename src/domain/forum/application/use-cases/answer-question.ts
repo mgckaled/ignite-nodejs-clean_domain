a9f7e816a1076683a363dee68362e9c1f22c4a42
mgckaled <marcelgckaled@gmail.com>
2023-11-19 19:13:20 -0300

    refactor: repositories tests

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -8,10 +8,18 @@ interface AnswerQuestionUseCaseRequest {
   content: string
 }
 
+interface AnswerQuestionUseCaseResponse {
+  answer: Answer
+}
+
 export class AnswerQuestionUseCase {
   constructor (private readonly answersRepository: AnswersRepository) {}
 
-  async execute ({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+  async execute ({
+    instructorId,
+    questionId,
+    content
+  }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
     const answer = Answer.create({
       content,
       authorId: new UniqueEntityID(instructorId),
@@ -20,6 +28,8 @@ export class AnswerQuestionUseCase {
 
     await this.answersRepository.create(answer)
 
-    return answer
+    return {
+      answer
+    }
   }
 }
